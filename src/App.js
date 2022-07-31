4330c7964b671a04f2d02e9da74fe0545b26a634
j-lindsey <j-lindsey@users.noreply.github.com>
2022-07-30 19:20:35 -0600

    fixed navigation bar and links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Home from "./pages/Home";
@@ -13,9 +13,10 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/universities" element={<Universities />}></Route>
+        <Route path="/postal" element={<PostalLookup />}></Route>
         <Route exact path="/" element={<Home />}>
-          <Route path="/universities" element={<Universities />}></Route>
-          <Route path="/postal" element={<PostalLookup />}></Route>
+          {" "}
         </Route>
       </Routes>
       <ToastContainer />
